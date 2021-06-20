62cab18dafd796cd3f98346196ac2b75eb82c50d
Andres Felipe Pardo Mesa <aanfepar@gmail.com>
2021-06-20 12:49:33 -0500

    add styles of dark mode header

--- components/header.tsx
+++ components/header.tsx
@@ -1,23 +1,31 @@
 import Image from 'next/image'
 import logo from '@/public/images/logo.png'
+import classNames from 'classnames'
 import styles from '@/styles/components/header.module.sass'
 
+interface HeaderProps {
+  dark: boolean,
+};
 
-export default function Header() {
+export default function Header({ dark }: HeaderProps) {
+
+  const headerClassNames = classNames(styles.Header, {
+    [styles.Header_dark]: dark
+  })
   return (
-    <header className={styles.Header}>
+    <header className={headerClassNames}>
       <figure>
-        <Image className={styles.Header_Logo} src={logo} alt="" placeholder="blur" />
+        <Image className={styles.Header__Logo} src={logo} alt="" placeholder="blur" />
       </figure>
-      <section className={styles.Header_Menu}>
-        <a className={styles.Header_Menu_Item}>About me</a>
-        <a className={styles.Header_Menu_Item}>Blog</a>
-        <a className={styles.Header_Menu_Item}>Projects</a>
-        <img className={styles.Header_Icon} src="/images/github-light.png" alt="" />
-        <img className={styles.Header_Icon} src="/images/linkedin-light.png" alt=""/>
-        <label className={styles.Header_Switch}>
-          <input className={styles.Header_Checkbox} type="checkbox"/>
-          <span className={styles.Header_Slider}>{"💡"}</span>
+      <section className={styles.Header__Menu}>
+        <a className={styles.Header__Item}>About me</a>
+        <a className={styles.Header__Item}>Blog</a>
+        <a className={styles.Header__Item}>Projects</a>
+        <img className={styles.Header__Icon} src={`/images/github-${dark ? 'light' :'dark'}.png`} alt="" />
+        <img className={styles.Header__Icon} src={`/images/linkedin-${dark ? 'light' :'dark'}.png`} alt="" />
+        <label className={styles.Header__Switch}>
+          <input type="checkbox" />
+          <span className={styles.Header__Slider}>{dark? "☀️": "🌙"}</span>
         </label>
       </section>
     </header>
