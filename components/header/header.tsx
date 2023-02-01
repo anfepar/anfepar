f853f500cbb1f36af64f4cb448c52368b2061995
Felipe Pardo <aanfepar@gmail.com>
2023-02-01 18:23:49 -0500

    feat(Header): :sparkles: add responsive menu of header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,13 +1,12 @@
-import STRINGS from '@/constants/string';
+import { useCallback, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslations } from '@/hooks/useTranslations';
-import logo from '@/public/images/logo.png';
 import styles from './header.module.css';
 
 const menuItems = [
-  { name: 'blog', text: 'blog', url: '/blog' },
-  { name: 'projects', text: 'projects', url: '/projects' },
+  { name: 'blog', url: '/blog' },
+  { name: 'projects', url: '/projects' },
   {
     name: 'github',
     url: 'https://github.com/anfepar',
@@ -24,25 +23,58 @@ const menuItems = [
 
 export const Header = () => {
   const { t } = useTranslations('Header');
-  return (
-    <header className={styles.Header}>
-      <Link href="/" className={styles.Header_Link}>
-        <Image className={styles.Header_Logo} src={logo} alt={t('logo_alt')} width={200} height={50} priority />
-      </Link>
-      <ul className={styles.Header_Menu}>
-        {menuItems.map((item) => {
-          const { name, url, icon, text } = item;
 
-          return (
-            <li className={styles.Header_Item} key={name}>
-              <Link className={styles.Header_Link} href={url} target={item.target || '_self'} rel="noreferrer">
-                {icon && <Image src={icon} width={25} height={25} alt={t('icon_alt', { name })} />}
-                {text && <p className={styles.Header_Text}>{t(`url_${name}`)}</p>}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </header>
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleOpenMenu = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
+  const getItems = useCallback(() => {
+    const socialMediaItems: any = [];
+    const links: any = [];
+
+    menuItems.forEach((item) => {
+      const { name, url, icon } = item;
+
+      const itemEntry = (
+        <Link className={`${styles.Header_Link} ${styles.Header_Link__SocialMedia}`} href={url} target={item.target || '_self'} rel="noreferrer">
+          {icon && <Image src={icon} width={25} height={25} alt={t('icon_alt', { name })} />}
+          {!icon && <p className={styles.Header_Text}>{t(`url_${name}`)}</p>}
+        </Link>
+      );
+
+      if (icon) socialMediaItems.push(itemEntry);
+      else links.push(<li className={styles.Header_Item} key={name}>{itemEntry}</li>);
+    });
+    return (
+      <>
+        {links}
+        <li className={styles.Header_Item}>
+          {socialMediaItems}
+        </li>
+      </>
+    );
+  }, [t]);
+
+  return (
+    <>
+      <header className={styles.Header}>
+        <Link href="/" className={styles.Header_Link}>
+          <Image className={styles.Header_Logo} src="/images/logo.png" alt={t('logo_alt')} width={200} height={50} priority />
+        </Link>
+        <ul className={styles.Header_Menu}>
+          {getItems()}
+        </ul>
+        <button className={styles.Header_Mobile_Menu}>
+          <Image src="/images/menu.png" alt={t('logo_alt')} onClick={handleOpenMenu} width={25} height={25} priority />
+        </button>
+      </header>
+      {menuIsOpen && (
+        <ul className={styles.Header_Mobile_List}>
+          {getItems()}
+        </ul>
+      )}
+    </>
   );
 };
