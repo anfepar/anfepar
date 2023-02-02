54f155bed3a05879a5e6301e357feb0e450484a7
Felipe Pardo <aanfepar@gmail.com>
2023-02-02 00:10:00 -0500

    style(Header): :recycle: show only necessary elements

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -10,52 +10,16 @@ const menuItems = [
     name: "github",
     url: "https://github.com/anfepar",
     icon: "/images/github.png",
-    target: "_blank"
   },
   {
     name: "linkedin",
     url: "https://www.linkedin.com/in/anfepar",
     icon: "/images/linkedin.png",
-    target: "_blank"
   },
 ];
 
 export const Header = () => {
   const { t } = useTranslations("Header");
-
-  const [menuIsOpen, setMenuIsOpen] = useState(false);
-
-  const handleOpenMenu = () => {
-    setMenuIsOpen(!menuIsOpen);
-  };
-
-  const getItems = useCallback(() => {
-    const socialMediaItems: any = [];
-    const links: any = [];
-
-    menuItems.forEach((item) => {
-      const { name, url, icon } = item;
-      const linkClasses = classNames(styles.Header_Link, { [styles.Header_Link__SocialMedia]: icon });
-
-      const itemEntry = (
-        <Link className={linkClasses} href={url} target={item.target || "_self"} rel="noreferrer">
-          {icon && <Image src={icon} width={25} height={25} alt={t("icon_alt", { name })} />}
-          {!icon && <p className={styles.Header_Text}>{t(`url_${name}`)}</p>}
-        </Link>
-      );
-
-      if (icon) socialMediaItems.push(itemEntry);
-      else links.push(<li className={styles.Header_Item} key={name}>{itemEntry}</li>);
-    });
-    return (
-      <>
-        {links}
-        <li className={styles.Header_Item}>
-          {socialMediaItems}
-        </li>
-      </>
-    );
-  }, [t]);
 
   return (
     <>
@@ -64,17 +28,19 @@ export const Header = () => {
           <Image className={styles.Header_Logo} src="/images/logo.png" alt={t("logo_alt")} width={180} height={45} priority />
         </Link>
         <ul className={styles.Header_Menu}>
-          {getItems()}
+          {menuItems.map((item, idx) => {
+            const { name, url, icon } = item;
+
+            return (
+              <li className={styles.Header_Item} key={`${name}_${idx}`}>
+                <Link className={styles.Header_Link} href={url} target="_blank" rel="noreferrer">
+                  {icon && <Image src={icon} width={25} height={25} alt={t("icon_alt", { name })} />}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
-        <button className={styles.Header_Mobile_Menu}>
-          <Image src="/images/menu.png" alt={t("logo_alt")} onClick={handleOpenMenu} width={25} height={25} priority />
-        </button>
       </header>
-      {menuIsOpen && (
-        <ul className={styles.Header_Mobile_List}>
-          {getItems()}
-        </ul>
-      )}
     </>
   );
 };
